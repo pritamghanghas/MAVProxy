8404c0154319ba0ae36f066f91e301b573d63651
Pritam Ghanghas <pritam_ghanghas@comcast.com>
2018-06-12 15:16:51 -0400

    picam triggers are sent to a locally running daemon. As this is too slow to do within the module and we still want to have images streamed even when not actually mapping

--- MAVProxy/modules/mavproxy_smartcamera/sc_picam.py
+++ MAVProxy/modules/mavproxy_smartcamera/sc_picam.py
@@ -11,15 +11,11 @@ Image size is held in the smart_camera.cnf
 import sys
 import time
 import math
-import cv2
 import sc_config
 import os
-from gi.repository import GExiv2
-from subprocess import call
-import picamera
+from requests import async
 
 class PiCam:
-
     def __init__(self, instance):
 
         # health
@@ -29,10 +25,6 @@ class PiCam:
         self.instance = instance
         self.config_group = "camera%d" % self.instance
 
-        # get image resolution
-        self.img_width = sc_config.config.get_integer(self.config_group,'width',3280)
-        self.img_height = sc_config.config.get_integer(self.config_group,'height',2464)
-        self.camera_name = sc_config.config.get_string(self.config_group, 'camera_name', 'picam')
         self.media_dir = os.path.expanduser(sc_config.config.get_string('general', 'media_dir', '~/media'))
         self.photos_dir = os.path.join(self.media_dir, self.camera_name, 'photos')
         self.video_dir = os.path.join(self.media_dir, self.camera_name, 'videos')
@@ -47,15 +39,12 @@ class PiCam:
         # background image processing variables
         self.img_counter = 0        # num images requested so far
 
-        self.camera = picamera.PiCamera()
-        self.camera.resolution = (self.img_width, self.img_height)
-
         # latest image captured
         self.latest_image = None
 
     # __str__ - print position vector as string
     def __str__(self):
-        return "SmartCameraWebCam Object W:%d H:%d" % (self.img_width, self.img_height)
+        return "SmartCameraWebCam Object "
 
     def boSet_GPS(self, mGPSMessage):
         if mGPSMessage.get_type() == 'GLOBAL_POSITION_INT':
@@ -63,18 +52,10 @@ class PiCam:
 
     # latest_image - returns latest image captured and write it to file
     def get_latest_image(self):
-        # write to file
-        #create folder for this session
         session_dir = os.path.join(self.photos_dir, str(self.instance))
-        if not os.path.exists(session_dir):
-            os.makedirs(session_dir)
         imgfilename = "img-%d.jpg" % (self.get_image_counter())
         complete_filename = os.path.join(session_dir,imgfilename)
-        print (complete_filename)
-        self.camera.capture(complete_filename)
-        exif = GExiv2.Metadata(complete_filename)
-        exif.set_gps_info(self.vehicleLon, self.vehicleLat, self.vehicleAMSL)
-        exif.save_file()
+        async.get('http://127.0.0.1:5555/capture?filename=%s&lat=%f&lon=%f&alt=%f' % (complete_filename,self.vehicleLat, self.vehicleLon, self.vehicleAMSL))
         return self.latest_image
 
     # get_image_counter - returns number of images captured since startup
@@ -98,7 +79,7 @@ class PiCam:
             if self.take_picture():
                 # display image
                 print("took one picture")
-#                cv2.imshow ('image_display', self.get_latest_image())
+                # cv2.imshow ('image_display', self.get_latest_image())
             else:
                 print "no image"
 
